29dfb003d2f1a98c9f3cd69a8ae8b4c1858fb280
Rebekah <139832391+rebekahktrevino@users.noreply.github.com>
2024-07-15 19:34:29 +0000

    my first rust program

---
+++ hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d
@@ -0,0 +1,5 @@
+/workspaces/Summer2024/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf: src/main.rs
+
+/workspaces/Summer2024/hello_world/target/debug/deps/hello_world-a58b90b4a373bcdf.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/workspaces/Summer2024/hello_world/target/debug/hello_world: /workspaces/Summer2024/hello_world/src/main.rs
