6a48bc553b7856ac3fbf43553dd88ce3ab42e40f
Rebekah <139832391+rebekahktrevino@users.noreply.github.com>
2024-07-26 03:17:51 +0000

    Assignment complete

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,50 +1,37 @@
 //Rebekah K. Trevino
 //3334-01
 
-fn is_even (n: i32) -> bool{
-    //checking if number is even if it is divisible by 2
-    n % 2 == 0
+use std::process::Command;
+
+fn executing_os_commands_linux(command_full:&str) {
+
+    let parts: Vec<&str> = command_full.split_whitespace().collect();
+    let actual_command = parts[0];
+    let arg1 = parts[1];
+
+    let output = Command::new(actual_command)
+        .arg(arg1)
+        .output()
+        .unwrap();
+        //Expect ("Failed to execute command");
+
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
 }
 
-fn main(){
-    // an array of 10 integer nums 
-    let numbers = [1,24,25,30,7,8,76,5,43,10];
+use std::io;
+use std::io::Read;
+use std::io::Write;
+//Self read write 
 
-    // to iterate through array
-    for &num in &numbers{
-        println!("Number:{}",num);
+fn accept_linux_command_from_user() -> String {
+    let mut buffer = String::new();
+    print!("Insert a Linux command to execute: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer.trim().to_string()
+}
 
-        //checking if even or odd
-        if is_even(num){
-            print!("EVEN:");
-        }
-        else{
-            print!("ODD:");
-        }
-        //check if divisible by 3 and 5
-        if num % 3 == 0{
-            print!("FIZZ");
-        }
-        if num % 5 == 0{
-            print!("BUZZ");
-        }
-        println!(); //new line 
-    }
-    //sum calculationusing while loop
-    let mut sum = 0;
-    let mut index = 0;
-    while index < numbers.len(){
-        sum += numbers[index];
-    index += 1;
-    } 
-    println!("Sum of all numbers:{}", sum);
-
-    //to find the largest num using a loop
-    let mut largest = numbers[0];
-    for &num in &numbers{
-        if num > largest{
-            largest = num;
-        }
-    }
-        println!("Largest number:{}", largest);
+fn main() {
+    let full_command:String = accept_linux_command_from_user();
+    executing_os_commands_linux(&full_command);
 }
